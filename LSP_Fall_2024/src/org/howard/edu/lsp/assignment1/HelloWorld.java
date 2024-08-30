dbe941a2a2d9d1588e103fd8fc1f3d08d1edd5aa
khanaemiller <khanaemiller@10.116.96.108>
2024-08-30 09:00:10 -0400

    First commit of assignment 1.

---
+++ LSP_Fall_2024/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * Name: Khanae' Miller
+ */
+
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!"); 
+	}
+
+}
